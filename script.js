1dbdd78f78294ca1578271ce19e976600a51d7e3
Your Name <yourname@example.com>
2026-01-08 17:27:54 +0545

    First Commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// Calculator 
+/* const display = document.getElementById("display");
+
+function appendToDisplay(input){
+  display.value += input; 
+}
+
+function clearDisplay(){
+  display.value ="";
+}
+
+function calculate(){
+  try{
+    display.value = eval(display.value);
+  }
+  catch(error){
+    display.value = "Error";
+  }
+}
+*/
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+  display.value += input;
+} 
+
+function clearDisplay(){
+  display.value ="";
+}
+
+function calculate(){
+  try{
+    display.value = eval(display.value);
+  }
+  catch(error){
+    display.value = "Error"
+  }
+}
